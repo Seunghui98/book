351c924bd3cd886386bc1d6f205009eaffbb5148
Seunghui98 <hithere1012@naver.com>
2022-06-29 16:11:36 +0900

    add spring security login

--- src/main/java/com/ellen/book/springboot/config/auth/domain/posts/BaseTimeEntity.java
+++ src/main/java/com/ellen/book/springboot/config/auth/domain/posts/BaseTimeEntity.java
@@ -1,11 +1,10 @@
-package com.ellen.book.springboot.domain.posts;
+package com.ellen.book.springboot.config.auth.domain.posts;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
